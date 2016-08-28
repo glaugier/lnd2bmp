359fbda5e34d6265d266c30b4fbe6b2639892676
glaugier <guillaume.laugier@gmail.com>
2016-08-28 19:23:47 -0300

    v0.02
    * Revoved some unused variables
    * Chose a lighter gray for the 2s (approx gray 60%)
    * Output no more in a ./bmp/ but straight into ./ (but filenames are no
    more truncated).

---
+++ devlog.md
@@ -0,0 +1,13 @@
+### v0.01
+First working version
+
+	* Hard-coded colors:
+		* 0: Forestgreen
+		* 1: Yellow
+		* 2: Gray (approx 40%)
+
+### v0.02
+
+	* Revoved some unused variables
+	* Chose a lighter gray for the 2s (approx gray 60%)
+	* Output no more in a `./bmp/` but straight into `./`
--- lnd2bmp.c
+++ lnd2bmp.c
@@ -41,7 +41,7 @@ void countStats(int **L, int count[9], int x, int y,
 				int x_size, int y_size, int NEonly);
 void split_path_file(char** p, char** f, char *pf);
 char *remove_ext(char* mystr, char dot, char sep);
-
+extern char *strdup(const char *s);
 
 typedef unsigned char byte;  /* just for convenience */
 
@@ -52,7 +52,7 @@ FILE *infile, *outFile;
 int x_size, y_size;  /* size of landscape */
 int r, g, b;  /* rgb values of landscape */
 
-char version[10] = "v0.01" ;
+char version[10] = "v0.02" ;
 
 /*
  * Main() routine.
@@ -74,48 +74,55 @@ printf("╚═══════════╝\n\n");
 		exit(EXIT_FAILURE);
 	}
 
+
+
 	// Get input name from first arg
 	char inputName[strlen(argv[1])+1];
 	strcpy(inputName, argv[1]);
 	// Create output name
-	int tmp=11+strlen(argv[1]);
-	char *fileName[strlen(argv[1])], *pathName[strlen(argv[1])]; // pointers
+	//~ char dirName[5] = "bmp";
+	char **fileName = malloc ( (strlen(argv[1])+10)*sizeof(char) );
+	char **pathName = malloc ( (strlen(argv[1])+10)*sizeof(char) );
+	char *outputName = malloc ( (strlen(argv[1])+10)*sizeof(char) );
 	split_path_file(&*pathName, &*fileName, argv[1]);
-	char *outputName = malloc ( (strlen(argv[1]) + 10)*sizeof(char) );	
-	char charFocal[3];
-	char dirName[6] = "bmp";
-	// Check if dir exists
-	DIR* dir = opendir(dirName);
-if (dir)
-{
-    /* Directory exists. */
-    closedir(dir);
-}
-else if (ENOENT == errno)
-{
-//printf("\nmouchard\n");
-    /* Directory does not exist. */
-printf("%s does not exist, creating it.\n", dirName);
-    mkdir(dirName, // Needs no final "/"
-			S_IRUSR | S_IRGRP | S_IROTH | S_IXUSR | S_IXGRP | S_IWUSR | S_IWGRP);
-			// Force permission to drwxr-xr--
-}
-else
-{
-    /* opendir() failed for some other reason. */
-    perror("Error while creating 'bmp/'.\n");
-}
-	outputName = strcat(dirName, "/"); // Update dirname
-	char *dirPathName = malloc ( (strlen(argv[1]) + strlen(dirName))*sizeof(char) );
+	char *dirPathName = malloc ( (strlen(argv[1]) + 3)*sizeof(char) );
+
+
+	
+	//~ // Check if dir exists
+	//~ DIR* dir = opendir(dirName);
+//~ if (dir)
+//~ {
+    //~ /* Directory exists. */
+    //~ closedir(dir);
+//~ }
+//~ else if (ENOENT == errno)
+//~ {
+//~ //printf("\nmouchard\n");
+    //~ /* Directory does not exist. */
+//~ printf("%s does not exist, creating it.\n", dirName);
+    //~ mkdir(dirName, // Needs no final "/"
+			//~ S_IRUSR | S_IRGRP | S_IROTH | S_IXUSR | S_IXGRP | S_IWUSR | S_IWGRP);
+			//~ // Force permission to drwxr-xr--
+//~ }
+//~ else
+//~ {
+    //~ /* opendir() failed for some other reason. */
+    //~ perror("Error while creating 'bmp/'.\n");
+//~ }
+	//~ outputName = strcat(dirName, "/"); // Update dirname
+	dirPathName[0] = '\0';
 	strcpy(dirPathName, *pathName);
-	dirPathName = strcat(dirPathName, dirName);
+	//~ dirPathName = strcat(dirPathName, dirName);
 	printf("fileName: %s\n", *fileName);
 	*fileName = remove_ext(*fileName, '.', '/');
-printf("Dirname is: %s\n", dirPathName);
-	strcpy(outputName, dirName);
-printf("5 outputname will be: %s %s\n", dirName, outputName);
-	outputName = strcat(outputName, *fileName);
-	outputName = strcat(outputName, ".bmp");
+//~ printf("Dirname is: %s\n", dirPathName);
+	//~ strcpy(outputName, dirName);
+	//~ outputName = strcat(outputName, *fileName);
+	//~ outputName = strcat(outputName, ".bmp");
+	//~ outputName[strlen(outputName)+1] = '\0';
+//~ printf("Outputname will be: %s\n", outputName);
+//~ printf("Outputname will be: %lu\n", strlen(outputName));
 
 
 infile=fopen(inputName, "r"); // Ouverture du fichier infile
@@ -155,16 +162,8 @@ printf("\tDetected %d lines and %d columns in %s\n", y_size, x_size, argv[1]);
 //int Lsize = x_size*y_size;
 int i, j;
 int** land=malloc(y_size*sizeof(int*));
-int** tags0=malloc(y_size*sizeof(int*));
-int** tags1=malloc(y_size*sizeof(int*));
-int** tags2=malloc(y_size*sizeof(int*));
-int** tagsm=malloc(y_size*sizeof(int*));
 for(i=0; i<y_size; ++i) {
 	land[i]=malloc(x_size*sizeof(int));
-	tags0[i]=malloc(x_size*sizeof(int));
-	tags1[i]=malloc(x_size*sizeof(int));
-	tags2[i]=malloc(x_size*sizeof(int));
-	tagsm[i]=malloc(x_size*sizeof(int));
 }
 infile=fopen(argv[1], "r"); // Ouverture du fichier infile
 	if( infile == NULL ) {
@@ -178,10 +177,6 @@ for(i=0; i<y_size; i++) {
 	for(j=0; j<x_size; j++) {
 		fscanf(infile,"%d;",&val);
 		land[i][j] = val;
-		tags0[i][j] = ( val == 0 ? 1 : 0 );
-		tags1[i][j] = ( val == 1 ? 1 : 0 );
-		tags2[i][j] = ( val == 2 ? 1 : 0 );
-		tagsm[i][j] = ( val == 0 ? 0 : 1 );
 	}
 }
 fclose(infile);
@@ -189,11 +184,13 @@ fclose(infile);
 
 
 int w=x_size, h=y_size; // TODO: homogeneize
-unsigned char *img = NULL;
+//~ printf("1==> Outputname will be: %s with %lu chars.\n", outputName, strlen(outputName)); // Gets truncated
+char *img = NULL;
+//~ printf("2==> Outputname will be: %s with %lu chars.\n", outputName, strlen(outputName));// Gets truncated
 int filesize = 54 + 3*w*h;  //w is your image width, h is image height, both int
-if( img )
-    free( img );
-img = (unsigned char *)malloc(3*w*h);
+//~ if( img ) free( img );
+img = (char *)malloc(3*w*h);
+//~ printf("3==> Outputname will be: %s with %d chars.\n", outputName, (int) (sizeof(outputName)/sizeof(char))); // Gets truncated
 memset(img,0,sizeof(img));
 
 int x, y;
@@ -202,9 +199,6 @@ for(int i=0; i<w; i++)
     for(int j=0; j<h; j++)
 {
     x=i; y=(h-1)-j;
-//    r = red[i][j]*255;
-//    g = green[i][j]*255;
-//    b = blue[i][j]*255;
 		if(land[i][j]== 0){
 			r = 34;
 			g = 139;  // Forestgreen
@@ -214,9 +208,9 @@ for(int i=0; i<w; i++)
 			g = 255; // Yellow (desaturated)
 			b = 25;
 		} else {
-			r = 100;
-			g = 100; // Just a random gray
-			b = 100;
+			r = 150;
+			g = 150; // Just a random gray
+			b = 150;
 	}
     if (r > 255) r=255;
     if (g > 255) g=255;
@@ -226,6 +220,7 @@ for(int i=0; i<w; i++)
     img[(x+y*w)*3+0] = (unsigned char)(b);
 }
 }
+//~ printf("==> Outputname will be: %s with %d chars.\n", outputName, (int) (sizeof(outputName)/sizeof(char))); // Gets even more truncated
 
 unsigned char bmpfileheader[14] = {'B','M', 0,0,0,0, 0,0, 0,0, 54,0,0,0};
 unsigned char bmpinfoheader[40] = {40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0};
@@ -244,6 +239,13 @@ bmpinfoheader[ 8] = (unsigned char)(       h    );
 bmpinfoheader[ 9] = (unsigned char)(       h>> 8);
 bmpinfoheader[10] = (unsigned char)(       h>>16);
 bmpinfoheader[11] = (unsigned char)(       h>>24);
+
+//~ strcpy(outputName, dirName);
+outputName = strcat(outputName, *fileName);
+outputName = strcat(outputName, ".bmp");
+outputName[strlen(outputName)+1] = '\0';
+//~ printf("Outputname will be: %s\n", outputName);
+//~ printf("Outputname length be: %lu\n", strlen(outputName));
 
 printf("Ready to save %s\n", outputName);
 outFile = fopen(outputName,"wb");
@@ -264,83 +266,17 @@ printf("\tSucces: statistics have been saved in: \n\t\t%s\n", outputName);
 
 	for(i=0; i<x_size; ++i) {
 		free(land[i]);
-		free(tags0[i]);
-		free(tags1[i]);
-		free(tags2[i]);
-		free(tagsm[i]);
 	}
 
 
 
 free(land);
-free(tags0);
-free(tags1);
-free(tags2);
-free(tagsm);
 
 
 	return 0;
 }  // fin du main()
 
 
-
-// Functions
-void countStats(int **L, int count[9], int x, int y,
-				int x_size, int y_size, int NEonly)
-{
-	int yN=y+1, yS=y-1, xE=x+1, xW=x-1;
-	if(yN>=y_size) yN=0;
-	if(xE>=x_size) xE=0;
-	if(yS<0) yS=y_size-1;
-	if(xW<0) xW=x_size-1;
-
-	// function for a given neighbour:
-	void nbTypeStep(const int selfValue, const int neighbourValue, int *count)
-	{
-		if(selfValue == 0){
-			count[0]++;
-			if(neighbourValue == 0) count[3]++;
-			if(neighbourValue == 1) count[4]++;
-			if(neighbourValue == 2) count[5]++;
-		}
-		if(selfValue == 1){
-			count[1]++;
-			if(neighbourValue == 0) count[4]++;
-			if(neighbourValue == 1) count[6]++;
-			if(neighbourValue == 2) count[7]++;
-		}
-		if(selfValue == 2){
-			count[2]++;
-			if(neighbourValue == 0) count[5]++;
-			if(neighbourValue == 1) count[7]++;
-			if(neighbourValue == 2) count[8]++;
-		}
-		if(selfValue !=0 && selfValue !=1 && selfValue !=2) {
-			puts("Error: (nbTypeStep) Pixel value was not in {0,1,2}!");
-			printf("Pixel value is: %d\n", selfValue);
-			//~ printLandscape(L, x_size, y_size);
-   	 	exit(EXIT_FAILURE);
-   	}
-		if(neighbourValue !=0 && neighbourValue !=1 && neighbourValue !=2) {
-			puts("Error: (nbTypeStep) Neighbour pixel value was not in {0,1,2}!");
-			printf("Pixel value is: %d\n", neighbourValue);
-
-			//~ printLandscape(L, x_size, y_size);
-   	 	exit(EXIT_FAILURE);
-   	}
-	}
-//	printf("Pixel coordinales: %d, %d val: %d\n", x, y, L[y][x]);
-//	printf("Neigbour pixel coordinales: %d, %d val: %d\n", x, yN, L[yN][x]);
-	nbTypeStep(L[y][x], L[yN][x], count); // N
-//	printf("Neigbour pixel coordinales: %d, %d val: %d\n", xE, y, L[y][xE]);
-	nbTypeStep(L[y][x], L[y][xE], count); // E
-	if(!NEonly) {
-//			printf("Neigbour pixel coordinales: %d, %d val: %d\n", x, yS, L[yS][x]);
-		nbTypeStep(L[y][x], L[yS][x], count); // S
-//			printf("Neigbour pixel coordinales: %d, %d val: %d\n", xW, y, L[y][xW]);
-		nbTypeStep(L[y][x], L[y][xW], count); // W
-	}
-}
 
 void split_path_file(char** p, char** f, char *pf) {
     char *slash = pf, *next;
